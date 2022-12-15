2011b068d4a00ddea0908785e47ecf9595932a9e
Harlen R. Compton <harlen@harlencompton.com>
2022-12-15 08:48:29 +0000

    fixing CSRF and object storage errors

--- src/zoneton_lodge/storage_backends.py
+++ src/zoneton_lodge/storage_backends.py
@@ -5,12 +5,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
+    querystring_auth = False
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
+    querystring_auth = False
 
 
 class PrivateMediaStorage(S3Boto3Storage):
